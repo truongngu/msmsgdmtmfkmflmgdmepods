4645077e56f7ba7535eed7caf39a14d0989169e2
truongngu2012@gmail.com <truongngu2012@gmail.com>
2015-01-23 11:46:04 +0000

    + Gameconfig + SpriteInfor, FrameInfor, CloneInfor: LoadFromXml

---
+++ EditorTestTool/SpriteEditors/FrameInfor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SpriteEditor
+{
+    public class FrameInfor
+    {
+        #region Field
+        string _name;
+        List<int> _frameContent = new List<int>();
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public List<int> FrameContent
+        {
+            get { return _frameContent; }
+            set { _frameContent = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void LoadFromXml(XmlNode element)
+        {
+            //get attribute name
+            Name = element.Attributes["name"].Value;
+            
+            //get frame content
+            string content = element.InnerText;
+
+            //split into List
+            string[] arr = content.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++) {
+                FrameContent.Add(int.Parse(arr[i]));
+            }
+        }
+        #endregion
+    }
+}
--- EditorTestTool/SpriteEditors/SpriteInfor.cs
+++ EditorTestTool/SpriteEditors/SpriteInfor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SpriteEditor
 {
@@ -21,6 +22,7 @@ namespace SpriteEditor
             get { return _x; }
             set { _x = value; }
         }
+        
         public Vector2(float x, float y)
         {
             _x = x;
@@ -101,7 +103,191 @@ namespace SpriteEditor
         }
     }
 
-    public class SpriteInfor
+    public class EntityInfor
     {
+        #region Field
+        string _texturePath;
+        string _className;
+
+        //SPRITE_ATTRIBUTE
+        float _fps;
+        float _width;
+        float _height;
+        int _numSpriteX;
+        int _numSpriteY;
+        int _totalSprite;
+        //SPRITE_ATTRIBUTE
+
+        //SHADER
+        string _vertexShader;
+        string _fragmentShader;
+        //SHADER
+
+        //FRAME - ANIMATION
+        int _numFrame;
+        List<FrameInfor> _frames = new List<FrameInfor>();
+        //FRAME - ANIMATION
+
+        //CLONE
+        int _numClone;
+        List<CloneInfor> _clones = new List<CloneInfor>();
+        //CLONE
+        #endregion
+
+        #region Properties
+        public string TexturePath
+        {
+            get { return _texturePath; }
+            set { _texturePath = value; }
+        }
+
+        public List<CloneInfor> Clones
+        {
+            get { return _clones; }
+            set { _clones = value; }
+        }
+
+        public int NumClone
+        {
+            get { return _numClone; }
+            set { _numClone = value; }
+        }
+
+        public List<FrameInfor> Frames
+        {
+            get { return _frames; }
+            set { _frames = value; }
+        }
+
+        public int NumFrame
+        {
+            get { return _numFrame; }
+            set { _numFrame = value; }
+        }
+
+        public string FragmentShader
+        {
+            get { return _fragmentShader; }
+            set { _fragmentShader = value; }
+        }
+
+        public string VertexShader
+        {
+            get { return _vertexShader; }
+            set { _vertexShader = value; }
+        }
+
+        public string ClassName
+        {
+            get { return _className; }
+            set { _className = value; }
+        }
+
+        public float Fps
+        {
+            get { return _fps; }
+            set { _fps = value; }
+        }
+
+        public float Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public float Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public int NumSpriteX
+        {
+            get { return _numSpriteX; }
+            set { _numSpriteX = value; }
+        }
+
+        public int NumSpriteY
+        {
+            get { return _numSpriteY; }
+            set { _numSpriteY = value; }
+        }
+
+        public int TotalSprite
+        {
+            get { return _totalSprite; }
+            set { _totalSprite = value; }
+        }
+        #endregion
+
+        #region Method
+        public void LoadFromXml(XmlNode element)
+        {
+            string textureXpath = "TEXTURE_PATH";
+            TexturePath = element.SelectSingleNode(textureXpath).InnerText;
+
+            string classXpath = "CLASS_NAME";
+            ClassName = element.SelectSingleNode(classXpath).InnerText;
+
+            string attrXpath = "SPRITE_ATTRIBUTE";
+            XmlNode attrEle = element.SelectSingleNode(attrXpath);
+
+            Fps = float.Parse(attrEle.Attributes["fps"].Value);
+            Width = float.Parse(attrEle.Attributes["width"].Value);
+            Height = float.Parse(attrEle.Attributes["height"].Value);
+            NumSpriteX = int.Parse(attrEle.Attributes["num_spriteX"].Value);
+            NumSpriteY = int.Parse(attrEle.Attributes["num_spriteY"].Value);
+            TotalSprite = int.Parse(attrEle.Attributes["total_sprite"].Value);
+
+            string shaderXpath = "SHADER";
+            attrEle = element.SelectSingleNode(shaderXpath);
+
+            VertexShader = attrEle.Attributes["vertex"].Value;
+            FragmentShader = attrEle.Attributes["fragment"].Value;
+
+            string frameXpath = "FRAME_INFO";
+            attrEle = element.SelectSingleNode(frameXpath);
+
+            NumFrame = int.Parse(attrEle.Attributes["num_frame"].Value);
+
+            string childFrame = "FRAME";
+            XmlNodeList frameNode = attrEle.SelectNodes(childFrame);
+
+            Frames = new List<FrameInfor>();
+            int size = frameNode.Count;
+            if (size > NumFrame)
+                size = NumFrame;
+            //foreach (XmlNode node in frameNode)
+            for (int i = 0; i < size;i++)
+            {
+                FrameInfor infor = new FrameInfor();
+                infor.LoadFromXml(frameNode[i]);
+
+                Frames.Add(infor);
+            }
+
+            string cloneXpath = "CLONE_INFO";
+            attrEle = element.SelectSingleNode(cloneXpath);
+
+            NumClone = int.Parse(attrEle.Attributes["num_clone"].Value);
+
+            Clones = new List<CloneInfor>();
+
+            string childClone = "CLONE";
+            XmlNodeList cloneNode = attrEle.SelectNodes(childClone);
+
+            size = cloneNode.Count;
+            if (size > NumClone)
+                size = NumClone;
+
+            for (int i = 0; i < size; i++)
+            {
+                CloneInfor clone = new CloneInfor();
+                clone.LoadFromXml(cloneNode[i]);
+
+                Clones.Add(clone);
+            }
+        }
+        #endregion
     }
 }
