6c2e053736dcb4168fb726a2bf2c10705a4c3131
ltgamehba <ltgamehba@gmail.com>
2015-01-26 03:11:29 +0000

    + Add moving sprite feature

--- branches/SpriteEditor/EditorTestTool/SpriteEditors/Global.cs
+++ branches/SpriteEditor/EditorTestTool/SpriteEditors/Global.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SpriteEditor
 {
@@ -15,6 +17,22 @@ namespace SpriteEditor
             if (propertitiesGrid != null)
             {
                 propertitiesGrid.RefreshPropertyList();
+            }
+        }
+
+        public static void ExpandCloneInfor(String cloneName)
+        {
+            DependencyObject root=propertitiesGrid.Parent;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(root);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+                if (child is TreeViewItem)
+                {
+                    var tree = (TreeViewItem)child;
+                    tree.IsExpanded = true;
+                }
             }
         }
     }
